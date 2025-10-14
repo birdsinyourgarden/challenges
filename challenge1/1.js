bc81909b7a6a1cc8ab600da468a585f41e17b3dd
birdsinyourgarden <ruizpaloma9@gmail.com>
2025-10-14 10:12:21 +0200

    challenge 1

---
+++ challenge1/1.js
@@ -0,0 +1,21 @@
+/** Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color.
+ * Haz una función que devuelva una lista con todas las ovejas que sean de color rojo
+ * y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+ */
+
+const listaOvejas = [{
+    nombre: "Dolly",
+    color: "blanca"
+},
+{
+    nombre: "Sheep",
+    color: "negra"
+},
+{
+    nombre:"Satán",
+    color: "roja"
+},
+{
+    nombre: "Demoni",
+    color: "roja"
+}];
