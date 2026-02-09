bba316959ba3572814ba4facc95e83d0163e257a
birdsinyourgarden <ruizpaloma9@gmail.com>
2026-02-09 11:16:46 +0100

    exercise 18

--- challenge18/18.js
+++ challenge18/18.js
@@ -14,3 +14,31 @@
  * - La función retornará un Boolean que indique si ha superado la carrera.
  * Para ello tiene que realizar la opción correcta en cada tramo de la pista.
  */
+
+
+function race(actions, track) {
+    let trackResult = track.split('');
+    let clear = true;
+
+    for (let i = 0; i < trackResult.length; i++) {
+        if (actions[i] === "run" && trackResult[i] === "|") {
+            trackResult[i] = "/";
+            clear = false;
+        } else if (actions[i] === "jump" && trackResult[i] === "_") {
+            trackResult[i] = "x";
+            clear = false;
+        }
+    }
+
+    console.log(trackResult.join(''));
+    return clear;
+}
+
+
+let actions = ["run", "jump", "run", "jump"];
+let track = "_|_|";
+console.log(race(actions, track));
+
+
+
+
